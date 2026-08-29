ea4a15a4c546f6298217e0380f0d737c0d02739a
semen power <semnupower.05@hotmail.com>
2026-08-28 18:51:33 -0600

    push

--- main/dataset.py
+++
@@ -1,9 +0,0 @@
-# clase 20220205 min 1:01:43
-import kagglehub
-import pandas as pd
-import re
-
-# Download latest version
-path = kagglehub.dataset_download("sohrabdaemi/discogs-database-all-release-data")
-
-print("Path to dataset files:", path) 
